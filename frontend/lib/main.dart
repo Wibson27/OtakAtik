b5191eda5c2a575db5c1463e83e6a8e07baf2e9d
Alma Nurul Salma <almanurulsalma@gmail.com>
2025-06-05 09:33:05 +0700

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/forum_discussion_post.dart';
+import 'package:frontend/screens/sign_up_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const ForumDiscussionPostScreen(),
+      home: const SignUpScreen(),
     );
   }
 }
