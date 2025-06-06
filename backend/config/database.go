63bf07a548cb2bb7c61a8d4537a44125dee1ccf9
Wibson27 <rifqiwibisono271002@gmail.com>
2025-06-07 00:00:51 +0700

    Routing

---
+++ backend/config/database.go
@@ -0,0 +1,48 @@
+// package config
+
+// import (
+// 	"fmt"
+// 	"backend/models"
+
+// 	"golang.org/x/crypto/bcrypt"
+// 	"gorm.io/driver/postgres"
+// 	"gorm.io/gorm"
+// )
+
+// const (
+// 	host     = "localhost"
+// 	port     = 5432
+// 	user     = "postgres"
+// 	password = "CrispyJoybox"
+// 	dbName   = "Tenang"
+// )
+
+// func DatabaseConnection() *gorm.DB {
+// 	dsn := fmt.Sprintf(
+// 		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+// 		user, password, host, port, dbName,
+// 	)
+
+// 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	return database
+// }
+
+// func CreateOwnerAccount(db *gorm.DB) {
+// 	hashedPasswordBytes, _ := bcrypt.GenerateFromPassword([]byte("123456"), bcrypt.DefaultCost)
+// 	owner := models.User{
+// 		Role:     "Admin",
+// 		Name:     "Owner",
+// 		Password: string(hashedPasswordBytes),
+// 		Email:    "owner@go.id",
+// 	}
+
+// 	if db.Where("email=?", owner.Email).First(&owner).RowsAffected == 0 {
+// 		db.Create(&owner)
+// 	} else {
+// 		fmt.Println("owner exist")
+// 	}
+// }
